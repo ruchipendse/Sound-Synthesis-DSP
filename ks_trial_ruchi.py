be4c64de47a0f41f31adc839c9510da39d314f68
ruchmuch <41779023+ruchmuch@users.noreply.github.com>
2020-04-09 18:58:40 +0530

    Different initialization of the ring buffer
    Moreover, added averaging function

---
+++ ks_trial_ruchi.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#creating a ring buffer
+ring_buff = []
+x = 0
+for string in range(6):
+        #initialise ring buffer with zeroes
+        ring_buffer.append(np.zeros(string_buffer_sizes[string], dtype=np.float32))
+print(ring_buff)
